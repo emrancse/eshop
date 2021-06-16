9df4bbc186ad0494057b78a70373dedda4781520
Emran Hosen <emranict@gmail.com>
2021-06-16 14:16:24 +0600

    create categories migration file

---
+++ app/Models/Category.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $table = 'categories';
+
+    protected $fillable = [
+
+        'name','slug','description','image','parent_id','featured','menu'
+    ];
+
+    protected $casts = [
+        'parent_id' => 'integer',
+        'featured' => 'boolean',
+        'menu'     => 'boolean'
+    ];
+
+
+}
