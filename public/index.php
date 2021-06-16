c32b16187750e5e5c0357d9cc04c0af92cba6e96
Emran Hosen <emranict@gmail.com>
2021-06-16 11:57:30 +0600

    backend template mastering completed

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+
 /*
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
@@ -31,7 +32,12 @@ if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
 |
 */
 
+
+
 require __DIR__.'/../vendor/autoload.php';
+
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -45,6 +51,7 @@ require __DIR__.'/../vendor/autoload.php';
 */
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
+
 
 $kernel = $app->make(Kernel::class);
 
